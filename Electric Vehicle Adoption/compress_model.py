3fa4147af1c40a647f7b635e76c52cfabe0b6e59
Adedeji Badmus <142028730+dejiofng@users.noreply.github.com>
2026-07-08 11:38:36 +0100

    Delete Electric Vehicle Adoption/compress_model.py

--- Electric Vehicle Adoption/compress_model.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import joblib
-
-with open("model.pickle", "rb") as f:
-    model = pickle.load(f)
-
-joblib.dump(model, "model.joblib", compress=9)
-
-print("Model compressed successfully!")
